0036d14631cd2e31d43d723f6385e3d86a258395
Lukasz Fiszer <lukasz.fiszer@gmail.com>
2014-02-03 17:56:36 +0100

    Bette connections

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ var positions = {
   "Jacek Wojciechowicz":[7,1.5],
   "Jolanta Zdziech-Naperty":[1,3],
   "Wojciech Bartelski":[3,1.5],
-  "Działka na placu Defilad": [5,5],
+  "Działka na placu Defilad": [4.5, 5,5],
   "Działka na placu Zamkowym": [3,7],
-  "Ogród Jordanowski na ul. Szarej":[5.5,6.5],
-  "Parking na Krakowskim Przedmieściu":[1,6],
+  "Ogród Jordanowski na ul. Szarej":[5.5,6],
+  "Parking na Krakowskim Przedmieściu":[1,7],
   "Kamienica przy ul. Kazimierzowskiej": [9.5,6],
   "Boisko na Foksal": [7,7],
-  "Kamienica na aleji Szucha": [8, 8],
+  "Kamienica na aleji Szucha": [8.5, 8],
   "Marek Mikos": [6.5, 3],
-  "Gimnazjum na ul. Twardej": [1.5, 5],
+  "Gimnazjum na ul. Twardej": [1.5, 6],
   "Pracownia Dawos": [7, 5],
   "Jakub Rudnicki": [8,4]
 }
@@ -70,42 +70,20 @@ var mugCurcle = svg.append("defs").append("clipPath")
   .append("circle")
     .attr("r", mugDiameter / 2);
 
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset(function(d){
-    if(d.genre === 'figure'){
-      return [-25,0]
-    }else{
-        var dx = d.target.x - d.source.x,
-            dy = d.target.y - d.source.y,
-            dr = Math.sqrt(dx * dx + dy * dy);
-            console.log(dy/2,dx/2,dr)
-      return [dy/2,0]
-    }
-  })
-  .html(function(d) {
-    var html = "<strong>" + d.name + "</strong><br/><br/>"+d.desc+"<br/><br/>";
-    if (d.url){
-      html += '<a href="'+d.url+'">' + d.url + '</a>';
-    }
-    return html
-  })
 
-var toggleTooltip = function(d){
-  var current = tip.current;
-  if(current  && current === d){
-    tip.hide(d);
-    tip.current = null;
-  }
-  if(current !== d){
-    tip.show(d);
-    tip.current = d;
-  }
-
-};
+var curvedLine = function(d) {
+  var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y,
+      dr = Math.sqrt(dx * dx + dy * dy);
+  return "M" +
+      d.source.x + "," +
+      d.source.y + "A" +
+      dr + "," + dr + " 0 0,1 " +
+      d.target.x + "," +
+      d.target.y;
+}
 
 
-svg.call(tip);
 
 queue()
 .defer(d3.csv, 'data/figures.csv', function(d) {
@@ -138,24 +116,78 @@ queue()
     link.target = {x: target.x, y:target.y}
   })
 
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset(function(data){
+      var group = links.filter(function (d) { return d === data })
+      var circle = group.select('circle').node();
+      if(data.genre === 'figure'){
+        return [-25,0]
+      }else{
+        var target = d3.event.target.getBBox();
+        var el = circle.getBBox();
+        return [el.y-target.y-25,0]
+        //return [target.x-el.x,target.y-el.y]
+      }
+    })
+    .html(function(d) {
+      var html = "<strong>" + d.name + "</strong><br/><br/>"+d.desc+"<br/><br/>";
+      if (d.url){
+        html += '<a href="'+d.url+'">' + d.url + '</a>';
+      }
+      return html
+    })
+    
+  svg.call(tip);
+
+
+  var toggleTooltip = function(d){
+    console.log('tool')
+    var current = tip.current;
+    if(current  && current === d){
+      tip.hide(d);
+      tip.current = null;
+    }
+    if(current !== d){
+      tip.show(d);
+      tip.current = d;
+    }
+  
+  };
+
+  /*
+    Links
+  */
   var links = svg.append("g")
     .attr("class", "g-links")
     .selectAll("g")
       .data(links)
-    .enter().append("path")
-      .attr("class", "link")
+    .enter().append("g")
+      .attr("class", "g-link")
+
+    links.append("path")
+      .attr("class", "link-outer")
+      .attr("d", d3.svg.diagonal())
       .on('click', toggleTooltip)
-      .attr("d", function(d) {
-        var dx = d.target.x - d.source.x,
-            dy = d.target.y - d.source.y,
-            dr = Math.sqrt(dx * dx + dy * dy);
-        return "M" +
-            d.source.x + "," +
-            d.source.y + "A" +
-            dr + "," + dr + " 0 0,1 " +
-            d.target.x + "," +
-            d.target.y;
-       })
+
+    links.append("path")
+      .attr("class", "link-inner")
+      .on('click', toggleTooltip)
+//       .attr("d", curvedLine)
+      .attr("d", d3.svg.diagonal())
+
+   links.append('circle')
+      .attr("class", "link-circle")
+      .attr("cx", function(d){
+        var path = this.parentNode.getElementsByClassName('link-inner')[0]
+        return path.getPointAtLength(path.getTotalLength()/2).x;
+      })
+     .attr("cy", function(d){
+        var path = this.parentNode.getElementsByClassName('link-inner')[0]
+        return path.getPointAtLength(path.getTotalLength()/2).y;
+      })
+      .attr("r", 8)
+      .on('click', toggleTooltip)
 
   var circles = svg.append("g")
       .attr("class", "g-figures")
@@ -170,7 +202,7 @@ queue()
       });
 
   circles.append("circle")
-      .attr("r", (mugDiameter / 2) + 4)
+      .attr("r", (mugDiameter / 2))
       .attr("class", 'figure-circle-outer')
 
   circles.append("circle")
@@ -186,9 +218,6 @@ queue()
       .attr("clip-path", "url(#g-mug-clip)")
       .style("pointer-events", "none")
 
-   circles
-      .attr('x', function(d) { return d.x })
-      .attr('y', function(d, i) { return d.y })
-      .on('click', toggleTooltip)
+   circles.on('click', toggleTooltip)
 
 });
